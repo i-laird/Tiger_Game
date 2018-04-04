e8cf80eca31677ed4d98bfaddccf667a60a5e453
benSepanski <34109796+benSepanski@users.noreply.github.com>
2018-04-04 15:36:46 -0500

    Add files via upload
    Should be able to play as tiger against men
    
    Todo: remove cout statements,  a feature that lets you know if you can capture this turn, and will move the men randomly if the program screws up so that you can continue playing (will notify you)

---
+++ Tiger-Game/Board.h
@@ -0,0 +1,29 @@
+#ifndef BOARD_H_INCLUDED
+#define BOARD_H_INCLUDED
+
+#include <set>
+#include <vector>
+#include <cmath>
+
+#include "game_runner.h"
+#include "function_api.h"
+
+using namespace std;
+
+typedef vector<Token_t> State;
+typedef pair<Token_t, set<Point_t>> Orderless_State; // tiger token and men pos
+typedef vector<vector<vector<vector<bool> > > > Board_Adj_Mat;
+
+Point_t make_point(int r, int c);
+
+class Board{
+    private:
+        Board_Adj_Mat adjacencies;
+        set<Point_t> tiger_cage;
+    public:
+        Board();
+        bool isVertex(Point_t p) const;
+        bool isValid(const State& s, const Move_t& m) const;
+};
+
+#endif // BOARD_H_INCLUDED
---
+++ Tiger-Game/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Play_Tiger.h"
+
+using namespace std;
+
+int main()
+{
+    Play_Tiger t;
+    t.play();
+    return 0;
+}
