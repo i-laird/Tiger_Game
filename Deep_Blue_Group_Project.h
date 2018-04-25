c58db00de1009d30bb0f7e179ab5380666334b90
Unknown <ben_sepanski@baylor.edu>
2018-04-25 13:20:53 -0500

    Commented Men_Mover

--- Deep_Blue_Group_Project.h
+++ Deep_Blue_Group_Project.h
@@ -767,18 +767,49 @@ const Point_t RIGHT = make_point(0, 1);
 const Point_t CAGE_ENTRANCE = make_point(4,4);
 const Point_t TIGER_START = make_point(2,4);
 
-/// TO-DO ---- ADD COMMENTS!!!
 class Men_Mover {
 protected:
     // about current state
     GameRunner game;
     Unordered_State current;
 public:
+    /*       Men_Mover(const State& s)
+     *
+     * description: creates a Men_Mover object, no members are set. Should be
+     *              overloaded to set current to s.
+     * return: none
+     * precondition: s is a valid State
+     * postcondition: a Men_Mover object is created
+     */
     Men_Mover(const State &s);
 
+    /*       next_move(State s)
+     *
+     * description: returns the Move_t which the men should perform in the
+     *              given state
+     * return: Move_t
+     * precondition: s is a valid State
+     * postcondition: this current should be set to s
+     */
     virtual Move_t next_move(State s);
+
+    /*       ~Men_Mover()
+     *
+     * description: destructor
+     * return: none
+     * precondition: none
+     * postcondition: this object is deleted
+     */
     virtual ~Men_Mover() = default;
 protected:
+    /*       execute_move()
+     *
+     * description: returns the Move_t which the men should perform given
+     *              that current is the current state
+     * return: Move_t
+     * precondition: current is a valid State
+     * postcondition: none
+     */
     virtual Move_t execute_move() = 0;
 };
 
